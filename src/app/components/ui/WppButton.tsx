ae131b4110935c63ba873270ff2cbe5a3e2ee9b0
Tomas Gil Amoedo <tomasgilamoedo@gmail.com>
2025-01-14 22:44:18 -0300

    open app, not browser src

--- src/app/components/ui/WppButton.tsx
+++ src/app/components/ui/WppButton.tsx
@@ -4,12 +4,9 @@ export const WppButton = () => {
   return (
     <a
       className="shadow-wspp-link fixed-bottom-dvh-5 z-50 inline-flex items-center justify-center gap-x-2 rounded-full bg-[#25D366] px-6 py-2"
-      data-action="open"
-      data-phone="5491149796361"
-      data-message="¡Hola! Tengo una consulta sobre jamones Luvianka."
-      role="button"
       target="_blank"
-      href="https://web.whatsapp.com/send?phone=1149796361&amp;text=%C2%A1Hola!%20Tengo%20una%20consulta%20sobre%20una%20terapia%20natural."
+      rel="noopener noreferrer"
+      href="https://wa.me/5491149796361?text=¡Hola!%20Tengo%20una%20consulta%20sobre%20jamones%20Luvianka."
     >
       <WhatsappIcon />
 
