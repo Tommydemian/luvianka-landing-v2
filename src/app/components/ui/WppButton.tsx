199a7a47955ea263af2ff4434c11759bab1b2fec
Tomas Gil Amoedo <tomasgilamoedo@gmail.com>
2025-02-27 17:35:33 -0300

    feat: wspp open web or local app based on resolutoin

--- src/app/components/ui/WppButton.tsx
+++ src/app/components/ui/WppButton.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+"use client"
+
+import React, { useEffect } from "react";
 import { WhatsappIcon } from "../icons/social-media";
+import { useMediaQuery } from "./hooks/useMediaQuery";
 export const WppButton = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  useEffect(() => console.log(isMobile, "isMobile"))
+
+  const whatsappLink = isMobile
+    ? "whatsapp://send?phone=5491136406941&text=¡Hola!%20Tengo%20una%20consulta%20sobre%20jamones%20Luvianka."
+    : "https://wa.me/5491136406941?text=¡Hola!%20Tengo%20una%20consulta%20sobre%20jamones%20Luvianka.";
+
+
   return (
     <a
       className="shadow-wspp-link fixed-bottom-dvh-5 z-50 inline-flex items-center justify-center gap-x-2 rounded-full bg-[#25D366] px-6 py-2"
       target="_blank"
       rel="noopener noreferrer"
-      href="https://wa.me/5491149796361?text=¡Hola!%20Tengo%20una%20consulta%20sobre%20jamones%20Luvianka."
+      href={whatsappLink}
     >
       <WhatsappIcon />
 
---
+++ src/app/components/ui/hooks/useMediaQuery.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export function useMediaQuery(query: string): boolean {
+  // Initialize state with current match status
+  const [matches, setMatches] = useState<boolean>(() => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const listener = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
+
+    if (mediaQueryList.addEventListener) {
+      mediaQueryList.addEventListener("change", listener);
+    } else {
+      // Fallback for older browsers
+      mediaQueryList.addListener(listener);
+    }
+
+    return () => {
+      if (mediaQueryList.removeEventListener) {
+        mediaQueryList.removeEventListener("change", listener);
+      } else {
+        mediaQueryList.removeListener(listener);
+      }
+    };
+  }, [query]);
+
+  return matches;
+}
